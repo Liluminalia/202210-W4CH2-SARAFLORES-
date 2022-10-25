60db5e24aed74e578a3ad454c6bc5e99e9ef1495
Liluminalia <peloazul4@gmail.com>
2022-10-25 22:09:28 +0200

    update4 to see netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import "./App.css";
 import { Paso1 } from "./components/Paso1";
 import "./components/Form.scss";
@@ -10,7 +10,7 @@ import "./components/Form.scss";
 function App() {
     // codigo comentado xq no recuerdo bien como hacer la herencia y sino no me funciona netlify
 
-    // const [counter, setCounter] = useState(0);
+    //const [counter, setCounter] = useState(0);
 
     // const handlerCounter = (item: number) => {
     //     setCounter(counter + item);
